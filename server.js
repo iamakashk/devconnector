970e98d0b05e9544551916fc79c331d9a10da17e
AKASH KAMBLE <kambleakashg@gmail.com>
2018-08-12 15:40:24 +0530

    redux authentication

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express  = require('express');
-const mongoose  = require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
@@ -8,39 +8,31 @@ const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
-//body parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//DB config
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-//connect to MongoDB
+// Connect to MongoDB
 mongoose
-  .connect(db , { useNewUrlParser: true })
-  .then(() => {
-    console.log('MongoDB Connected');
-  }).catch(err => {
-    console.log(err)
-  });
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-// app.get('/' , (req, res) => {
-//   res.send('Hello');
-// });
-
-//Passport Middleware
+// Passport middleware
 app.use(passport.initialize());
 
 // Passport Config
 require('./config/passport')(passport);
 
-//Use routes
-app.use('/api/users' ,  users);
-app.use('/api/profile' ,  profile);
-app.use('/api/posts' ,  posts);
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-const port  = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-})
+app.listen(port, () => console.log(`Server running on port ${port}`));
